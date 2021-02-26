580ca4d6d06b52b98cf93bbba4374f0aef6b2dcc
Fredrick Wampamba <fredrickwampamba1@gmail.com>
2021-02-26 13:36:19 +0300

    changes made

--- income.php
+++ income.php
@@ -2,13 +2,14 @@
 // header('Content-Type: application/json');
 $conn = mysqli_connect('localhost',"root","","rest_api");
 if ($_SERVER["REQUEST_METHOD"] === 'POST')
-{
-    $reason = $_REQUEST["reason"];
-    $amount = $_REQUEST["amount"];
-    $date = $_REQUEST["date"];
-    $query = "INSERT INTO income (`reason`,`amount`,`date`)   VALUES('$reason','$amount','$date')";
+{   
+    $jsonData = json_decode($_POST);
+    $reason = $jsonData["name"];
+    $amount = $jsonData["amount"];
+    $date = $jsonData["date"];
+    $query = "INSERT INTO income (`name`,`amount`,`date`)   VALUES('$reason','$amount','$date')";
     $result = $conn->query($query);
-    if ($result == 1)
+    if ($result)
     {
         $data["message"] = "Saved";
         $data["status"] = "Ok";
