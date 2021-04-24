c6a942913307bddee80d601465d3810fb3d506a0
FredrickWampamba <fredrickwampamba1@gmail.com>
2021-04-24 23:52:55 +0300

    snackbar message

--- app/src/main/java/com/codiumug/android/MaterialDesignEx.java
+++ app/src/main/java/com/codiumug/android/MaterialDesignEx.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MaterialDesignEx extends AppCompatActivity {
 
@@ -14,6 +17,16 @@ public class MaterialDesignEx extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material_design_ex);
+
+        RelativeLayout parent = findViewById(R.id.parent);
+        Button displaySnackBar = findViewById(R.id.button1);
+        displaySnackBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //We are going to display Snack Bar here
+                Snackbar.make(parent,"You just displayed the snack bar", Snackbar.LENGTH_INDEFINITE).show();
+            }
+        });
 
         FloatingActionButton floatingActionButton = findViewById(R.id.floatButton);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
