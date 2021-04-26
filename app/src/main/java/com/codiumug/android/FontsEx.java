17c91082f2b799e5d3e7124f05f1875d2ac6ec5f
FredrickWampamba <fredrickwampamba1@gmail.com>
2021-04-26 18:13:27 +0300

    Fonts Activity Added

--- app/src/main/java/com/codiumug/android/FontsEx.java
+++ app/src/main/java/com/codiumug/android/FontsEx.java
@@ -1,8 +1,14 @@
 package com.codiumug.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FontsEx extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class FontsEx extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fonts_ex);
+
+        TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button1);
+
+        Typeface typeface = ResourcesCompat.getFont(this,R.font.cabin_sketch);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setTypeface(typeface);
+                Toast.makeText(FontsEx.this, "You have Changed the Font Face", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
